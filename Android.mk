df79783591e69ebec3f18b0bf8a97080d627d471
ljz <liujianzhang_1985@163.com>
2014-12-05 10:21:15 +0800

    add Android.mk

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2011 QUALCOMM Incorporated.
+# All Rights Reserved.
+# Qualcomm Confidential and Proprietary.
+# Developed by QRD Engineering team.
+#
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 andbase
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
+	src/com/yixi/window/service/IService.aidl
+
+LOCAL_PACKAGE_NAME := MainActivity
+
+
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := andbase:libs/andbase.jar
+
+include $(BUILD_MULTI_PREBUILT)
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
